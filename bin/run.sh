976d6e4713a8c100e90b8de24a90b92046697210
Hendri Pretorius <pretorh@gmail.com>
2018-06-05 21:57:13 +0200

    use symlink to setup sites/ dir
    if the dir exists, then do nothing
    else symlink the mounted dir into the nginx dir

--- bin/run.sh
+++ bin/run.sh
@@ -7,5 +7,12 @@ if [ ! -f ./private/dhparam.pem ] ; then
   sh $(dirname $0)/generate-dhparam.sh
 fi
 
+if [ ! -d /etc/nginx/conf.d/sites/ ] ; then
+ echo "creating sym link from mounted sites to nginx dir"
+ mkdir -pv ./private/sites
+ ln -sv $(pwd)/private/sites /etc/nginx/conf.d/sites
+fi
+
+echo ""
 echo "starting nginx"
 nginx -g 'daemon off;'
