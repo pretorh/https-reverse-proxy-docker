53a0519dd135cf928cccd855a6629a596f3a205b
Hendri Pretorius <pretorh@gmail.com>
2018-09-15 15:38:15 +0200

    cron: fix command, log output to /var/log/rproxy

--- bin/job/install.sh
+++ bin/job/install.sh
@@ -3,7 +3,8 @@ set -e
 echo "installing cron job for $CRON_DAY dow, $CRON_HOUR hour"
 
 min=$(date '+%M')
-cmd=$(pwd)/job/renew-job.sh
-echo "$min $CRON_HOUR * * $CRON_DAY $cmd" | crontab -
+cmd=$(pwd)/job/renew-and-reload.sh
+mkdir -pv /var/log/rproxy/
+echo "$min $CRON_HOUR * * $CRON_DAY $cmd >> /var/log/rproxy/renew-and-reload-job.log 2>&1" | crontab -
 crontab -l
 crond -L /var/cron.log -l 0
