645e8e0f133eeab174a96b8b61a755ad3f5b5f93
Hendri Pretorius <pretorh@gmail.com>
2018-09-15 12:20:52 +0200

    exit renew script with failure if nothing renewed, success if some

--- bin/renew.sh
+++ bin/renew.sh
@@ -1,4 +1,12 @@
+renewed=0
+
 for d in $(ls -1 ./private/acme | grep -v private) ; do
   echo $d
-  sh ./lets-encrypt.sh $d
+  sh ./lets-encrypt.sh $d && renewed=1
 done
+
+if [ "$renewed" -eq 1 ] ; then
+  echo "certificates were renewed"
+  exit 0
+fi
+exit 1
