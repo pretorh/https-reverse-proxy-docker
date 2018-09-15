75cc13a2c404ed0f3947248bc6883e6d1b0b5d3e
Hendri Pretorius <pretorh@gmail.com>
2018-09-15 16:16:11 +0200

    cron: run a shell script after certificates renewed in job

--- bin/job/renew-and-reload.sh
+++ bin/job/renew-and-reload.sh
@@ -2,9 +2,13 @@
 set -e
 
 cd $(dirname $0)/..
+updated=0
 if sh ./renew-certs.sh ; then
   echo "reloading"
   ./reload-nginx.sh
+  updated=1
 else
   echo "up to date"
 fi
+
+[ -x ./private/post-cron.sh ] && ./private/post-cron.sh $updated
