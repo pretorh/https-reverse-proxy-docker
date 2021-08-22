4986e1ec7a16a693e9fb67ce2c956ea375d998a3
Hendri Pretorius <pretorh@gmail.com>
2021-08-22 15:09:00 +0200

    certs: add helper scripts to issue, install scripts from acme.sh

---
+++ bin/install-cert.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+set -e
+
+domain=${1?need the domain name as the first parameter}
+
+echo "rproxy: installing cert for $domain"
+acme.sh --install-cert -d "'$domain'" \
+  --key-file "/etc/ssl/acme/private/$domain/privkey.pem" \
+  --fullchain-file "/etc/ssl/acme/$domain/fullchain.pem" \
+  --reloadcmd /opt/rproxy/reload-nginx.sh
