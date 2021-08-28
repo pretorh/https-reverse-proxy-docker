dc6c5235064522250cea7a1bf45f688a84d090c1
Hendri Pretorius <pretorh@gmail.com>
2021-08-28 07:29:12 +0200

    scripts: fix quotes in issue, install scripts

--- bin/install-cert.sh
+++ bin/install-cert.sh
@@ -4,7 +4,7 @@ set -e
 domain=${1?need the domain name as the first parameter}
 
 echo "rproxy: installing cert for $domain"
-acme.sh --install-cert -d "'$domain'" \
+acme.sh --install-cert -d "$domain" \
   --key-file "/etc/ssl/acme/private/$domain/privkey.pem" \
   --fullchain-file "/etc/ssl/acme/$domain/fullchain.pem" \
   --reloadcmd /opt/rproxy/reload-nginx.sh
--- bin/setup/issue.sh
+++ bin/setup/issue.sh
@@ -4,4 +4,4 @@ set -e
 domain=${1?need the domain name as the first parameter}
 
 echo "rproxy: issuing for $domain"
-acme.sh --issue -d "'$domain'" --server Letsencrypt.org -w /var/www/acme/ --log
+acme.sh --issue -d "$domain" --server Letsencrypt.org -w /var/www/acme/ --log
