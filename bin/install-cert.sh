f107eea8c09015dea89ae975b805c02032ed92d7
Hendri Pretorius <pretorh@gmail.com>
2021-08-28 07:32:27 +0200

    install: create directories for ssl certs when installing

--- bin/install-cert.sh
+++ bin/install-cert.sh
@@ -3,6 +3,9 @@ set -e
 
 domain=${1?need the domain name as the first parameter}
 
+mkdir -pv "/etc/ssl/acme/$domain/"
+mkdir -pv "/etc/ssl/acme/private/$domain/"
+
 echo "rproxy: installing cert for $domain"
 acme.sh --install-cert -d "$domain" \
   --key-file "/etc/ssl/acme/private/$domain/privkey.pem" \
