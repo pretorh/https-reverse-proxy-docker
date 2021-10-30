2ebd6cd2b16c2fdcfa5cd9be93809f0550c386ed
Hendri Pretorius <pretorh@gmail.com>
2021-10-30 07:32:26 +0200

    cleanup: remove old acme-client helper script

--- bin/lets-encrypt.sh
+++
@@ -1,6 +0,0 @@
-host=${1?need hostname as the first parameter}
-AGREEMENT=https://letsencrypt.org/documents/LE-SA-v1.2-November-15-2017.pdf
-
-mkdir -pv /etc/ssl/acme/private
-
-acme-client -a $AGREEMENT -Nnmv $host
