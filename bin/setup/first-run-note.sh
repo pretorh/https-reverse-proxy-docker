a8a1e5d8a84971207489a46087b9c52b76bf897e
Hendri Pretorius <pretorh@gmail.com>
2021-10-30 07:33:49 +0200

    update how-to note for issuing, installing cert

--- bin/setup/first-run-note.sh
+++ bin/setup/first-run-note.sh
@@ -2,4 +2,5 @@ echo "This seems to be the first run, or you are missing the ssl certificates"
 echo "make sure $(pwd)/private is mounted"
 echo "to create the certificates, run:"
 id=$(cat /proc/self/cgroup | grep 'docker' | sed 's/^.*\///' | tail -n1)
-echo "  docker exec $id sh ./lets-encrypt.sh ..."
+echo "  docker exec $id sh ./setup/issue.sh <domain>"
+echo "  docker exec $id sh ./install-cert.sh <domain>"
