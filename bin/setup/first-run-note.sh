9f0e1b9ce17761602aa2feaeabee99b862dc8235
Hendri Pretorius <pretorh@gmail.com>
2021-10-30 07:35:11 +0200

    cleanup: first-run-note
    add shebang, fix usless cat (cat ... | grep -> grep ...)

--- bin/setup/first-run-note.sh
+++ bin/setup/first-run-note.sh
@@ -1,6 +1,8 @@
+#!/usr/bin/env sh
+
 echo "This seems to be the first run, or you are missing the ssl certificates"
 echo "make sure $(pwd)/private is mounted"
 echo "to create the certificates, run:"
-id=$(cat /proc/self/cgroup | grep 'docker' | sed 's/^.*\///' | tail -n1)
+id=$(grep 'docker' /proc/self/cgroup | sed 's/^.*\///' | tail -n1)
 echo "  docker exec $id sh ./setup/issue.sh <domain>"
 echo "  docker exec $id sh ./install-cert.sh <domain>"
