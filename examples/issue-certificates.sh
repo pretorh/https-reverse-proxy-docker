f02104a964070d4e18bd65e79e06f15be96e1f36
Hendri Pretorius <pretorh@gmail.com>
2023-05-05 07:41:52 +0200

    docs: extract first run issuing example to script in examples

---
+++ examples/issue-certificates.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+
+# example command for isssuing certificates, for the domains used in sites/*.conf
+docker run --rm -ti \
+    -p 80:80 \
+    -v "$(pwd)/.private":/opt/rproxy/private \
+    pretorh/https-reverse-proxy:latest \
+    ./domain-setup.sh simple.domain1.example.com web.domain1.example.com combined.domain1.example.com
