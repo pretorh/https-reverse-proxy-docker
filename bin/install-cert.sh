cb8ed461097fbace00fac177c6f2dc202d057215
Hendri Pretorius <pretorh@gmail.com>
2023-06-10 18:26:52 +0200

    change location of private key files (breaking change)
    move and rename private key files from
    `/etc/ssl/acme/private/$domain/privkey.pem` ->
    `/etc/ssl/acme/$domain/private-key.pem`

--- bin/install-cert.sh
+++ bin/install-cert.sh
@@ -8,6 +8,6 @@ mkdir -pv "/etc/ssl/acme/private/$domain/"
 
 echo "rproxy: installing cert for $domain"
 acme.sh --install-cert -d "$domain" \
-  --key-file "/etc/ssl/acme/private/$domain/privkey.pem" \
+  --key-file "/etc/ssl/acme/$domain/private-key.pem" \
   --fullchain-file "/etc/ssl/acme/$domain/fullchain.pem" \
   --reloadcmd /opt/rproxy/reload-nginx.sh
