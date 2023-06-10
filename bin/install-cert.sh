55dc413b3f9ad2f3e990e47fb01624c96b96bb27
Hendri Pretorius <pretorh@gmail.com>
2023-06-10 18:38:16 +0200

    allow not reloading nginx after installing certificates

--- bin/install-cert.sh
+++ bin/install-cert.sh
@@ -6,8 +6,13 @@ domain=${1?need the domain name as the first parameter}
 mkdir -pv "/etc/ssl/acme/$domain/"
 mkdir -pv "/etc/ssl/acme/private/$domain/"
 
+reloadcmd=/opt/rproxy/reload-nginx.sh
+if [ "$NO_RELOAD" ] ; then
+  reloadcmd=true
+fi
+
 echo "rproxy: installing cert for $domain"
 acme.sh --install-cert -d "$domain" \
   --key-file "/etc/ssl/acme/$domain/private-key.pem" \
   --fullchain-file "/etc/ssl/acme/$domain/fullchain.pem" \
-  --reloadcmd /opt/rproxy/reload-nginx.sh
+  --reloadcmd "$reloadcmd"
