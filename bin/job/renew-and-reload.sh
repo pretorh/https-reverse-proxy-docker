b5923fdd22dc3a73612cf44b9706211a39d6f3c1
Hendri Pretorius <pretorh@gmail.com>
2023-08-28 16:35:56 +0200

    renew: prefix echo message, reword and add more details
    prefix all echo messages in `renew-and-reload.sh` with the script
    filename
    
    reword renew messages

--- bin/job/renew-and-reload.sh
+++ bin/job/renew-and-reload.sh
@@ -2,26 +2,30 @@
 set -e
 
 start_file="$(mktemp)"
+log_prefix=$(basename "$0")
 
 updated=0
 if sh /opt/rproxy/renew-certs.sh ; then
-  echo "update job successful"
+  echo "$log_prefix: renew script successful"
   if find /etc/ssl/acme/ -type f -name 'private-key.pem' -newer "$start_file" | grep '^' ; then
+    echo "$log_prefix: some private key files were updated"
     updated=1
+  else
+    echo "$log_prefix: no private key files were updated"
   fi
 else
-  echo "error renewing!" >&2
+  echo "$log_prefix: error renewing!" >&2
   updated=-1
 fi
 
 if [ $updated -eq 1 ] ; then
-  echo "certificates renewed, reloading"
+  echo "$log_prefix: certificates renewed, reloading"
   /opt/rproxy/reload-nginx.sh
 fi
 
 if [ -x /opt/rproxy/private/post-cron.sh ] ; then
-  echo "executing post-cron.sh script"
+  echo "$log_prefix: executing post-cron.sh script with updated=$updated"
   /opt/rproxy/private/post-cron.sh $updated
 else
-  echo "no post-cron.sh script"
+  echo "$log_prefix: no post-cron.sh script"
 fi
